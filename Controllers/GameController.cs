c0c84646e8719874ca7d72fb65d9faac361a43e2
mariedale3 <marie.dale@education.gov.uk>
2020-02-25 15:18:17 +0000

    Further adjustments - Nickname with validation check

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DAS_Capture_The_Flag.ViewModels;
+using DAS_Capture_The_Flag.Views.Game;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DAS_Capture_The_Flag.Controllers
@@ -29,8 +31,22 @@ namespace DAS_Capture_The_Flag.Controllers
             return View();
         }
 
-        public IActionResult SetNickname() // --TEMP
-        { 
+        [HttpPost]
+        public async Task<IActionResult> SetNicknamePost(SetNickNameViewModel vm) { 
+           // var view = viewmodel;
+           if (!ModelState.IsValid)
+           {
+               return View("SetNickname");
+
+            }
+
+           return RedirectToAction("FindGame", "Game",);
+           // return View();
+        }
+
+        [HttpGet]
+        public IActionResult SetNickname()
+        {
             return View();
         }
 
